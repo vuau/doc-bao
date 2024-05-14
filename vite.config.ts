00dc5cbee90e4a38f2ed9a9fc5cb3533ea998333
Au <phamvuau@gmail.com>
2024-05-14 12:37:42 +0700

    update icon

--- vite.config.ts
+++ vite.config.ts
@@ -27,12 +27,12 @@ export default defineConfig({
         id: "/",
         icons: [
           {
-            src: "images/android-chrome-512x512.png",
+            src: "images/maskable_icon_x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "images/android-chrome-192x192.png",
+            src: "images/maskable_icon_x192.png",
             sizes: "192x192",
             type: "image/png",
           },
